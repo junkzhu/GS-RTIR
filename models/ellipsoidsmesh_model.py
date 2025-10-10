affa81b41cf4ccadf2e2ad12eead8be24b97183a
junkzhu <rookiebrianz@gmail.com>
2025-10-10 12:17:24 +0800

    add save ply interface

--- models/ellipsoidsmesh_model.py
+++ models/ellipsoidsmesh_model.py
@@ -15,15 +15,6 @@ class EllipsoidsFactory:
         self.metallics = []
 
     def add(self, mean, scale, sigmat=1.0, albedo=[0.5, 0.5, 0.5], quaternion=[0.0, 0.0, 0.0, 0.0], normal=[0.0, 0.0, 1.0], roughness = 0.8, metallic = 0.0, feature=np.zeros((16, 3))):
-        sigmat = 1 / (1 + np.exp(-sigmat))
-        sigmat = np.clip(sigmat, 1e-8, 1.0 - 1e-8)
-        scale = np.exp(scale)
-        scale = np.maximum(scale, 1e-6)    
-        quaternion /= np.linalg.norm(quaternion, keepdims=True)
-        quaternion = np.roll(quaternion, shift=-1)
-        normal /= np.linalg.norm(normal, keepdims=True)
-        #normal = [x * 2 - 1 for x in normal]
-
         self.centers.append(mi.ScalarPoint3f(mean))
         self.scales.append(mi.ScalarVector3f(scale))
         self.quaternions.append(mi.ScalarQuaternion4f(quaternion))
@@ -50,8 +41,8 @@ class EllipsoidsFactory:
 
         self.albedos = np.array(self.albedos).reshape((num_gaussians, -1))
         albedos = mi.TensorXf(np.array(self.albedos))
-        roughnesses = mi.TensorXf(self.roughnesses, shape=(num_gaussians, 1))
-        metallics = mi.TensorXf(self.metallics, shape=(num_gaussians, 1))
+        roughnesses = mi.TensorXf(np.array(self.roughnesses))
+        metallics = mi.TensorXf(np.array(self.metallics))
         
         attributes = {
             'centers': centers,
@@ -75,8 +66,11 @@ class EllipsoidsFactory:
         normal = gaussians._normal.detach().numpy()
         features = gaussians._features_dc.detach().numpy()
         feature_rests = gaussians._features_rest.detach().numpy()
+        albedo = gaussians._albedo.detach().numpy()
+        roughness = gaussians._roughness.detach().numpy()
+        metallic = gaussians._metallic.detach().numpy()
         
-        for xyz, opacity, scale, rot, normal, feature, feature_rest in zip(xyzs, opacitys, scales, rots, normal, features, feature_rests):            
+        for xyz, opacity, scale, rot, normal, feature, feature_rest, albedo, roughness, metallic in zip(xyzs, opacitys, scales, rots, normal, features, feature_rests, albedo, roughness, metallic):            
             self.add(
                 mean=xyz.tolist(),
                 scale=scale.tolist(),
@@ -85,6 +79,9 @@ class EllipsoidsFactory:
                 feature=np.concatenate((feature, feature_rest), axis=0).tolist(),
                 
                 normal=normal.tolist(),
+                albedo=albedo.tolist(),
+                roughness=roughness.tolist(),
+                metallic=metallic.tolist()
             )
 
         return self.build()
--- models/gaussian_model.py
+++ models/gaussian_model.py
@@ -108,6 +108,23 @@ class GaussianModel:
             normals = np.zeros((xyz.shape[0], 3))
             normals[:, 2] = 1.0
 
+        if all(n in prop_names for n in ["a_0", "a_1", "a_2"]):
+            albedos = np.stack((np.asarray(plydata.elements[0]["a_0"]),
+                                np.asarray(plydata.elements[0]["a_1"]),
+                                np.asarray(plydata.elements[0]["a_2"])), axis=1)
+        else:
+            albedos = np.ones((xyz.shape[0], 3), dtype=np.float32) * 0.5
+
+        if "r" in prop_names:
+            roughnesses = np.asarray(plydata.elements[0]["r"])[..., np.newaxis]
+        else:
+            roughnesses = np.ones((xyz.shape[0], 1), dtype=np.float32)
+
+        if "m" in prop_names:
+            metallics = np.asarray(plydata.elements[0]["m"])[..., np.newaxis]
+        else:
+            metallics = np.zeros((xyz.shape[0], 1), dtype=np.float32)
+        
         self._xyz = torch.from_numpy(xyz)
         self._opacity = torch.from_numpy(opacities)    
         self._scaling = torch.from_numpy(scales)
@@ -115,26 +132,58 @@ class GaussianModel:
         self._features_dc = torch.from_numpy(features_dc)
         self._features_rest = torch.from_numpy(features_rest) 
         self._normal = torch.from_numpy(normals) 
+        self._albedo = torch.from_numpy(albedos)
+        self._roughness = torch.from_numpy(roughnesses)
+        self._metallic = torch.from_numpy(metallics)
+
+        self._opacity = torch.sigmoid(self._opacity)
+        self._opacity = torch.clamp(self._opacity, 1e-8, 1.0 - 1e-8)
+
+        self._scaling = torch.exp(self._scaling)
+        self._scaling = torch.clamp(self._scaling, min=1e-6)
+
+        self._rotation = torch.roll(self._rotation, shifts=-1, dims=1)
+        self._rotation = self._rotation / (torch.norm(self._rotation, dim=1, keepdim=True) + 1e-8)
+        
+        self._normal = self._normal / (torch.norm(self._normal, dim=1, keepdim=True) + 1e-8)
+        #normal = [x * 2 - 1 for x in normal]
 
     def restore_from_params(self, params):
-        data = np.array(params['shape.data'])
-        n = data.shape[0] // 10
-        data = data.reshape(n, 10)
+        n = self._xyz.shape[0]
+
+        if 'shape.data' in params:
+            data = np.array(params['shape.data'])
+            data = data.reshape(n, 10)
+            
+            self._xyz = data[:, 0:3]
+            self._scaling = data[:, 3:6]
+            self._rotation = data[:, 6:10]
+
+        if 'shape.sh_coeffs' in params:
+            sh_coeffs = np.array(params['shape.sh_coeffs'])
+            sh_coeffs = sh_coeffs.reshape(n, -1)
+            self._features_dc   = sh_coeffs[:, 0:3]
+            self._features_rest = sh_coeffs[:, 3:]
+
+        if 'shape.opacities' in params:
+            opacities = np.array(params['shape.opacities'])
+            self._opacity = opacities.reshape(n, 1)
         
-        self._xyz = data[:, 0:3]
-        self._scaling = data[:, 3:6]
-        self._rotation = data[:, 6:10]
+        if 'shape.normals' in params:
+            normals = np.array(params['shape.normals'])
+            self._normal = normals.reshape(n, 3)
 
-        sh_coeffs = np.array(params['shape.sh_coeffs'])
-        sh_coeffs = sh_coeffs.reshape(n, -1)
-        self._features_dc   = sh_coeffs[:, 0:3]
-        self._features_rest = sh_coeffs[:, 3:]
-
-        opacities = np.array(params['shape.opacities'])
-        self._opacity = opacities.reshape(n, 1)
-
-        normals = np.array(params['shape.normals'])
-        self._normal = normals.reshape(n, 3)
+        if 'shape.albedos'in params:
+            albedos = np.array(params['shape.albedos'])
+            self._albedo = albedos.reshape(n, 3)
+        
+        if 'shape.roughnesses' in params:
+            roughnesses = np.array(params['shape.roughnesses'])
+            self._roughness = roughnesses.reshape(n, 1)
+        
+        if 'shape.metallics' in params:
+            metallics = np.array(params['shape.metallics'])
+            self._metallic = metallics.reshape(n, 1)
 
     def save_ply(self, path):
         N = self._xyz.shape[0]
@@ -144,9 +193,12 @@ class GaussianModel:
             ('nx', 'f4'), ('ny', 'f4'), ('nz', 'f4'),
             ('opacity', 'f4'),
             ('f_dc_0', 'f4'), ('f_dc_1', 'f4'), ('f_dc_2', 'f4'),
+            ('a_0', 'f4'), ('a_1', 'f4'), ('a_2', 'f4'),
+            ('r', 'f4'),
+            ('m', 'f4')
         ]
-
-        num_coeffs = self._features_rest.shape[1]
+        
+        num_coeffs = self._features_rest.reshape(N, -1).shape[1]
         for i in range(num_coeffs):
             dtype.append((f'f_rest_{i}', 'f4'))
 
@@ -158,9 +210,10 @@ class GaussianModel:
 
         vertices = np.zeros(N, dtype=dtype)
 
-        vertices['x'] = self._xyz[:, 0].astype(np.float32)
-        vertices['y'] = self._xyz[:, 1].astype(np.float32)
-        vertices['z'] = self._xyz[:, 2].astype(np.float32)
+        xyzs = self._xyz.numpy() if isinstance(self._xyz, torch.Tensor) else self._xyz
+        vertices['x'] = xyzs[:, 0].astype(np.float32)
+        vertices['y'] = xyzs[:, 1].astype(np.float32)
+        vertices['z'] = xyzs[:, 2].astype(np.float32)
 
         normals = self._normal.numpy() if isinstance(self._normal, torch.Tensor) else self._normal
         normals = normals / (np.linalg.norm(normals, axis=1, keepdims=True) + 1e-8)
@@ -169,11 +222,10 @@ class GaussianModel:
         vertices['nz'] = normals[:, 2].astype(np.float32)
 
         opacity = self._opacity.numpy() if isinstance(self._opacity, torch.Tensor) else self._opacity
-        opacity = np.clip(opacity, 1e-6, 1-1e-6)
-        opacity = np.log(opacity / (1 - opacity))
         vertices['opacity'] = opacity[:, 0].astype(np.float32)
 
         features_dc = self._features_dc.numpy() if isinstance(self._features_dc, torch.Tensor) else self._features_dc
+        features_dc = features_dc.reshape(N, -1)
         vertices['f_dc_0'] = features_dc[:, 0].astype(np.float32)
         vertices['f_dc_1'] = features_dc[:, 1].astype(np.float32)
         vertices['f_dc_2'] = features_dc[:, 2].astype(np.float32)
@@ -194,6 +246,17 @@ class GaussianModel:
         rots = np.roll(rots, shift=1, axis=1)
         for i in range(rots.shape[1]):
             vertices[f'rot_{i}'] = rots[:, i].astype(np.float32)
+
+        albedos = self._albedo.numpy() if isinstance(self._albedo, torch.Tensor) else self._albedo
+        vertices['a_0'] = albedos[:, 0].astype(np.float32)
+        vertices['a_1'] = albedos[:, 1].astype(np.float32)
+        vertices['a_2'] = albedos[:, 2].astype(np.float32)
+
+        roughness = self._roughness.numpy() if isinstance(self._roughness, torch.Tensor) else self._roughness
+        vertices['r'] = roughness[:, 0].astype(np.float32)
+
+        metallic = self._metallic.numpy() if isinstance(self._metallic, torch.Tensor) else self._metallic
+        vertices['m'] = metallic[:, 0].astype(np.float32)
 
         ply_el = PlyElement.describe(vertices, 'vertex')
         PlyData([ply_el], text=False).write(path)
